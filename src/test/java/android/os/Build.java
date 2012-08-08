cc1bfd1ea1476d112b852bc02a86adc5e5c7c7f9
tafypz <sebastien.astie@gmail.com>
2012-08-08 14:22:21 -0400

    Created android.os.Build mock object for junit tests (referenced in spring framework android jar files)

---
+++ src/test/java/android/os/Build.java
@@ -0,0 +1,11 @@
+package android.os;
+
+public class Build {
+	public static class VERSION {
+		public static final int SDK_INT = 9;
+	}
+	
+	public static class VERSION_CODES {
+		public static final int GINGERBREAD = 9;
+	}
+}
