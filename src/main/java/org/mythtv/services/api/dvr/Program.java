0aa43577dc080cc4d0384da0d4f024fa4c44c99d
Daniel Frey <dfrey@knoll.com>
2012-10-11 11:28:50 -0400

    added equals and hashcode

--- src/main/java/org/mythtv/services/api/dvr/Program.java
+++ src/main/java/org/mythtv/services/api/dvr/Program.java
@@ -487,6 +487,42 @@ public class Program implements Serializable, Comparable<Program> {
 	}
 
 	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ( ( title == null ) ? 0 : title.hashCode() );
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals( Object obj ) {
+		if( this == obj ) {
+			return true;
+		}
+		if( obj == null ) {
+			return false;
+		}
+		if( getClass() != obj.getClass() ) {
+			return false;
+		}
+		Program other = (Program) obj;
+		if( title == null ) {
+			if( other.title != null ) {
+				return false;
+			}
+		} else if( !title.equals( other.title ) ) {
+			return false;
+		}
+		return true;
+	}
+
+	/* (non-Javadoc)
 	 * @see java.lang.Object#toString()
 	 */
 	@Override
